791985169a8176fbb9040503c03119ff4949e461
Anthony Sutton <anthony.sutton95@gmail.com>
2019-09-19 13:15:55 -0700

    Added Project Manager object and console.log for tests. Getting undefined error on listsSubjects method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,7 +8,7 @@ class Person {
   }
 
   speak() {
-    return `Hello, my name is ${this.name}, I am from ${this.location}.`;
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
   }
 }
 
@@ -38,9 +38,13 @@ class Student extends Person {
   }
 
   listsSubjects() {
-    this.favSubjects.forEach(function(subject) {
-      console.log(subject);
-    });
+    // this.favSubjects.forEach(function(subject) {
+    //   console.log(subject);
+    // });
+
+    for (let i = 0; i < this.favSubjects.length; i++) {
+      console.log(this.favSubjects[i]);
+    }
   }
 
   PRAssignment(subject) {
@@ -49,6 +53,22 @@ class Student extends Person {
 
   sprintChallenge(subject) {
     return `${this.name} has begun sprint challenge on ${subject}.`;
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(attr) {
+    super(attr);
+    this.gradClassName = attr.className;
+    this.favInstructor = attr.favInstructor;
+  }
+
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`;
   }
 }
 
@@ -70,7 +90,25 @@ const anthony = new Student({
   catchPhrase: "Life happens",
   previousBackground: "Manager at a contact center",
   className: "WEB24",
-  favSubjects: ["HTML", "CSS", "Javascript"]
+  favSubjects: ["HTML", "CSS", "JavaScript"]
 });
 
-console.log(anthony.sprintChallenge("test"));
+const don = new ProjectManager({
+  name: "Donald",
+  location: "Louisiana",
+  age: 41,
+  favLanguage: "JavaScript",
+  specialty: "React",
+  catchPhrase: "Trust the process",
+  gradClassName: "WEB12",
+  favInstructor: "Brit Hemming"
+});
+
+console.log(anthony.speak());
+console.log(fred.demo("React"));
+console.log(fred.grade(anthony, "React"));
+console.log(anthony.listsSubjects());
+console.log(anthony.PRAssignment("React"));
+console.log(anthony.sprintChallenge("React"));
+console.log(don.standUp("web_lecture"));
+console.log(don.debugsCode(anthony, "React"));
