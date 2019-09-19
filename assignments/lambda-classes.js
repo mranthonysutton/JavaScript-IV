147f6962192b76a6bbfbf1d35d7d649ff59fe998
Anthony Sutton <anthony.sutton95@gmail.com>
2019-09-19 12:40:53 -0700

    Built People Class and Intructor class that extends People.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+
+class People {
+  constructor(attr) {
+    this.name = attr.name;
+    this.location = attr.location;
+    this.age = attr.age;
+  }
+
+  speak() {
+    return `Hello, my name is ${this.name}, I am from ${this.location}.`;
+  }
+}
+
+class Instructor extends People {
+  constructor(attr) {
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}.`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}.`;
+  }
+}
